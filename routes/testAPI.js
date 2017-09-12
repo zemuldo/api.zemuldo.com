fef57a1abaff948f599c7c9b5e7754ddbdf2a667
Danstan Otieno Onyango <danstan.otien@gmail.com>
2017-09-12 15:49:32 +0300

    adds endpoint for filter content

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,7 +3,7 @@
 let express = require("express");
 const EventEmitter = require('events');
 
-let {getAllBlogs,getBlog,addNewBlog,getBlogs} = require('../tools/database')
+let {getAllBlogs,getBlog,addNewBlog,getBlogs,getFilterBlogs} = require('../tools/database')
 let router = express();
 
 router.use((req, res, next) => {
@@ -31,13 +31,16 @@ router.get(('/posts/:type/:title'), async function (req, res) {
     res.send(state)
 })
 router.get(('/posts/:type'), async function (req, res) {
-    console.log("------------------------------------"+req.params.type)
     let state = await getBlogs(req.params.type)
     console.log(state)
     res.send(state)
 })
 router.get(('/all'), async function (req, res) {
     let state = await getAllBlogs()
+    res.send(state)
+})
+router.get(('/filter/:filterString'), async function (req, res) {
+    let state = await getFilterBlogs(req.params.filterString)
     res.send(state)
 })
 module.exports = router;
