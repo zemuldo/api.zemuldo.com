ebef18fa94c143b16fb38b251d2f75009c5bab2f
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-15 03:55:20 +0300

    limit fetch all

--- tools/database.js
+++ tools/database.js
@@ -156,10 +156,12 @@ module.exports = {
                     reject(e)
                 }
                 else {
-                    for(let i=0;i<10;i++){
+                    let filtered = []
+                    for(let i=0;i<o.length;i++){
                         o[i].body = o[i].body.slice(0,140)
+                        filtered.push(o[i])
                     }
-                    resolve(o)
+                    resolve(filtered)
                 }
             });
         })
