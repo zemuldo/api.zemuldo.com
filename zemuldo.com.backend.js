339b0e987b1881dc1e7f1aea5ae7a40bbb1fbfa2
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-20 17:22:13 +0300

    enforce header control

--- zemuldo.com.backend.js
+++ zemuldo.com.backend.js
@@ -2,6 +2,8 @@
 
 let express = require("express");
 let bodyParser = require('body-parser');
+let helmet = require('helmet')
+let checkMe = require('cookie-session')
 let config = require('./config/env');
 
 let configs = {port:8090}
@@ -9,11 +11,35 @@ let configs = {port:8090}
 let app = express();
 
 app.use(bodyParser.json());
+app.use(helmet())
+app.set('x-powered-by',false)
+app.set('X-Powered-By',false)
+app.use(helmet.ieNoOpen())
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet({
+    frameguard: false,
+    noCache:true
+}))
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+let expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
+app.use(checkMe({
+    name: 'session',
+    keys: ['themati#@tripple26=n26gohb()@#$$#$THF$%^$FGDFRFU', '#$THF$%^$FGDFRFU26gohb()@#i#@tripple26='],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        domain: 'zemuldo.com',
+        path: '/',
+        expires: expiryDate
+    }
+}))
 
-//testAPI Route
-let testAPI = require('./routes/testAPI');
+let route = require('./routes');
 
-app.use(testAPI);
+app.use(route);
 
 app.listen(configs.port, "0.0.0.0",function(){
     console.log("Web server running at http://localhost:"+configs.port)
