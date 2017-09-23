29f2f8f44b99a77c105c532f687164e6f76a0d30
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-23 17:06:03 +0300

    defines the pots schema

---
+++ schemas/index.js
@@ -0,0 +1,27 @@
+'use strict'
+module.exports = {
+
+    post:{
+        title:{
+            type: 'string',
+            maxLen:40,
+            minLen:6
+        },
+        body:{
+            type:'string',
+            maxLen:1500,
+            minLen:400
+        },
+        type:{
+            type:'string',
+            dev:'Developer Articles. Related to Programming and Software Development',
+            tech:'Technology based articles like BigData and AI',
+            business:'Business articles',
+            reviews:'Reviews',
+            tuts:'Tutorials'
+            },
+        author:{
+            type:'string'
+        }
+    },
+}
