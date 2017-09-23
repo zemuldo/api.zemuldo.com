5405e86949a9580fed1c1d5d093eed9aba299094
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-23 17:54:24 +0300

    updates env configs

--- config/env.js
+++ config/env.js
@@ -1,12 +1,8 @@
 module.exports = {
     live:{
-        port:9320,
         httpPort:8090,
-        address:"172.16.112.7"
     },
     dev:{
-        port:9320,
         httpPort:8090,
-        address:"127.0.0.1"
     }
 }
