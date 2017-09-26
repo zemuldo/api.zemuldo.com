56d818f336c9ad77ab9c06724be0890e44002239
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2017-09-27 00:16:33 +0300

    updates insert and get one db util

--- tools/controllers.js
+++ tools/controllers.js
@@ -1,12 +1,48 @@
 'use strict'
 let {addReview,getFilterBlogs,getAllBlogs,getBlog,addNewBlog,getBlogs,addVisitor} = require('./database')
+let {validatePost} = require('../tools/utilities')
 
 
 module.exports = {
     newPost:data =>{
+        return new Promise(async function (resolve,reject) {
+            let validate = await validatePost(data)
+            if(!validate.error){
+                let state  = addNewBlog(data)
+                resolve(state)
+            }
+            else{
+                reject(validate)
+            }
+        })
+            .then(function (succeed) {
+                return succeed
+            })
+            .catch(function (err) {
+                return err
+            })
+    },
+    getPost:data =>{
         return new Promise(function (resolve,reject) {
             if(data){
-                let state  = addNewBlog(data)
+                let state  = getBlog(data.queryParam)
+                resolve(state)
+            }
+            else {
+                reject(false)
+            }
+        })
+            .then(function (succeed) {
+                return succeed
+            })
+            .catch(function (err) {
+                return err
+            })
+    },
+    getPosts:data =>{
+        return new Promise(function (resolve,reject) {
+            if(data){
+                let state  = getBlogs(data.queryParam)
                 resolve(state)
             }
             else {
