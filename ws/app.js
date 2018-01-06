100f4664832034adcad13e55589e43f166714c28
Danstan Otieno Onyango <danstan.otieno@gmail.com>
2018-01-06 16:28:24 +0300

    adds blogs mess handlers

--- ws/app.js
+++ ws/app.js
@@ -2,49 +2,85 @@ const server = require('../server/app.js');
 const processRequest = require('./intents');
 const WebSocket = require('ws');
 const uuidv4 = require('uuid/v4');
+const {titles} = require('../db/db');
+const {topics} = require('../config/env')
 
 const wss = new WebSocket.Server({server: server});
 
 let sessions = {};
 
 wss.on('connection', (ws) => {
-  let sessionId = uuidv4();
+    let sessionId = uuidv4();
 
-  setTimeout(function () {
-      if(!sessions[sessionId]){
-          if(ws.readyState===1){
-              ws.send(JSON.stringify({"type":"bot","msg":"Hi its good to have you here, You can find more info about me through my profile bot."}));
-          }
-      }
-      else {
-          console.log(sessions)
-      }
-  },30000);
+    setTimeout(function () {
+        if (!sessions[sessionId]) {
+            if (ws.readyState === 1) {
+                ws.send(JSON.stringify({
+                    pups: 'bot',
+                    type: 'bot',
+                    msg: 'Hi its good to have you here, You can find more info about me through my profile bot.'
+                }));
+            }
+        }
+        else {
+            //console.log(sessions)
+        }
+    }, 30000);
 
-  ws.on('message', function (msg) {
-          sessions[sessionId] = {
-              id:sessionId,
-              date:new Date()
-          };
-          if(JSON.parse(msg).sessionId && !sessions[JSON.parse(msg).sessionId].messages){
-              sessions[JSON.parse(msg).sessionId].messages=true
-          }
-          processRequest(msg)
-              .then(function (answer) {
-                  if(ws.readyState===1){
-                      ws.send(JSON.stringify({type: 'bot', msg: answer}))
-                  }
-              })
-              .catch(function (err) {
-                  console.log(err)
-              })
-  }
+    ws.on('message', function (msg) {
+            let o = JSON.parse(msg);
+            sessions[sessionId] = {
+                id: sessionId,
+                date: new Date()
+            };
+            if (o.sessionId && !sessions[o.sessionId].messages) {
+                sessions[o.sessionId].messages = true
+            }
+            console.log(o.pups)
+            switch (o.pups) {
+                case 'chat':
+                    processRequest(msg)
+                        .then(function (answer) {
+                            if (ws.readyState === 1) {
+                                ws.send(JSON.stringify({type: 'bot', pups: 'bot', msg: answer}))
+                            }
+                        })
+                        .catch(function (err) {
+                            console.log(err)
+                        });
+                    break;
+                case 'exploreBlogs':
+                    titles.find().limit(10).toArray()
+                        .then(function (o) {
+                            ws.send(JSON.stringify({type: 'exploreBlogs', pups: 'exploreBlogs', msg: o}))
+                        })
+                        .catch(function () {
 
-  );
-  ws.on('error',(err)=>{
-    console.log(err)
-  });
-  ws.send(JSON.stringify({type: 'sessionId', msg: sessionId}));
+                        })
+                    break;
+                case 'topicDetails':
+                    for(let i=0;i<topics.length;i++){
+                        titles.find({topics:topics[i].key}).count()
+                            .then(function (n) {
+                                if(n>0){
+                                    ws.send(JSON.stringify({type: 'topicDetails', pups: 'topicDetails', msg: [{key:topics[i].key,blogs:n}]}))
+                                }
+                            })
+                            .catch(function (e) {
+                                console.log(e)
+                            })
+                    }
+                    break;
+                case 'blog':
+                    break;
+
+            }
+        }
+    );
+    ws.on('error', (err) => {
+        console.log(err)
+    });
+    ws.send(JSON.stringify({type: 'sessionId', msg: sessionId}));
 });
 
 module.exports = wss;
