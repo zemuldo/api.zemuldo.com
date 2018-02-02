deaa0f64e3231c9e5063dd4210eb73aac5a7696c
Danstan Oteino Onyango <danstan.otieno@gmail.com>
2018-02-02 22:20:18 +0300

    rename

---
+++ photos.zemuldo.com/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        };
+    };
+    componentDidMount(){}
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+export default App
