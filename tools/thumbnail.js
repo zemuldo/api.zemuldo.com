a325f2be258e1b6ff8b65dbdfafee0decd75c77f
Danstan Onyango <danstan.otieno@gmail.com>
2018-05-20 20:46:27 +0300

    thumbnail dynamic unbluur

--- tools/thumbnail.js
+++ tools/thumbnail.js
@@ -6,13 +6,24 @@ const logger = require('./logger')
 module.exports = {
     resize: (inputStream, format, toWidth, toHeight,blurL, blurR) => {
         return new Promise((resolve, reject) => {
-            return gm(inputStream)
+            if(blurL===0){
+                return gm(inputStream)
+                .resize(toWidth, toHeight)
+                .toBuffer(format, function (err, buffer) {
+                    if (err) reject({error:'<p>thumbnail error</p>'});
+                    resolve(buffer)
+                })
+            }
+            else {
+                return gm(inputStream)
                 .resize(toWidth, toHeight)
                 .blur(blurL, blurR)
                 .toBuffer(format, function (err, buffer) {
                     if (err) reject({error:'<p>thumbnail error</p>'});
                     resolve(buffer)
                 })
+            }
+           
         })
             .then(o => o)
             .catch(e => {
