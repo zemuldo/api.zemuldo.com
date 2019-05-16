421a6766ebf4867046d020c1eb9cfb29b04952a1
Danstan Onyango <danstan.otieno@gmail.com>
2019-05-16 23:09:17 +0300

    adds pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,20 +11,8 @@ module.exports = {
           env_production: {
               "PORT": 8090,
               "NODE_ENV": "production",
-          }
-        },
-        {
-            name: "Zemuldo API Cluster 1 8091",
-            script: "./app.js",
-            watch: true,
-            env: {
-                "PORT": 8091,
-                "NODE_ENV": "development"
-            },
-            env_production: {
-                "PORT": 8091,
-                "NODE_ENV": "production",
-            }
+          },
+          instances:'2'
         }
     ]
   }
