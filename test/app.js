4aefb5a37f0adffc4b14c75a2e6ccbb1201fe784
Danstan Onyango <danstan.otieno@gmail.com>
2019-10-17 00:44:57 +0300

    adds mocha test to ci

--- test/app.js
+++ test/app.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const app = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expectedResponse = require('../express/definitions/responses')
+const expectedResponse = require('../express/definitions/responses');
 
 chai.use(chaiHttp);
 const requester =chai.request(app);
