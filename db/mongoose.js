51e0639895b7cd3ba4a38e0e31843b8af4bb1c08
Danstan Onyango <danstan.otieno@gmail.com>
2019-11-06 10:18:26 +0300

    fix url when not in prod

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,8 +4,8 @@ const logger = require('../tools/logger');
 require('dotenv').config();
 
 function connectionString (){
-  if (process.env.NODE_ENV === 'production') `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}`;
-  else `mongodb://${process.env.DB_HOST}/${process.env.DATABASE}`;
+  if (process.env.NODE_ENV === 'production') return `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}`;
+  else return `mongodb://${process.env.DB_HOST}/${process.env.DATABASE}`;
 }
 
 mongoose.connect(connectionString(), {useNewUrlParser: true});
