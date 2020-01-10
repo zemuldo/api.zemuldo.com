8d990dbee13d4cd3857eac7d6850d3efadb280cf
Danstan Onyango <danstan.otieno@gmail.com>
2020-01-10 23:26:20 +0300

    adds unique tags

--- db/models/tag.js
+++ db/models/tag.js
@@ -2,6 +2,7 @@ const mongoose =  require('../mongoose.js');
 
 const Schema = mongoose.Schema;
 const tagSchema = new Schema({
+  _id: {type: String, required: true},
   label: { type: String, required: true},
   color: { type: String, required: false},
   value: { type: String, required: true, unique: true, dropDups: true},
