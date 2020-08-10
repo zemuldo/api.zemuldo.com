9d5b88390f4449f00f6c635661ca69a51b274af1
Danstan <danstan.otieno@gmail.com>
2020-08-10 06:53:36 +0300

    adds cors plug and change local ex server port

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :phoenix_app, PhoenixAppWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 8091],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
